c9c31e1e28b7cb0d6dbedc045123cdf31e8ce18e
Tazgo <tasgo@outlook.com>
2016-01-15 14:59:45 -0500

    Initial commit

---
+++ app/src/main/java/org/metrobots/botcv/CvUtils.java
@@ -0,0 +1,8 @@
+package org.metrobots.botcv;
+
+/**
+ * Created by Tasgo on 1/15/16.
+ */
+public class CvUtils {
+
+}
---
+++ app/src/main/java/org/metrobots/botcv/MainActivity.java
@@ -0,0 +1,46 @@
+package org.metrobots.botcv;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceView;
+
+import org.opencv.android.CameraBridgeViewBase.*;
+import org.opencv.android.JavaCameraView;
+import org.opencv.core.Mat;
+
+public class MainActivity extends AppCompatActivity implements CvCameraViewListener {
+    private boolean paused = false;
+    private CycleRunnable cycleRunnable;
+    private JavaCameraView cameraView;
+
+    static {
+        System.loadLibrary("opencv_java3");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cameraView = (JavaCameraView) findViewById(R.id.cameraView);
+        cameraView.setVisibility(SurfaceView.VISIBLE);
+        cameraView.setCvCameraViewListener(this);
+        cameraView.enableView();
+    }
+
+    public void onCameraViewStarted(int width, int height) {
+    }
+
+    public void onCameraViewStopped() {
+    }
+
+    @Override
+    public Mat onCameraFrame(Mat inputFrame) {
+        return inputFrame;
+    }
+
+    @Override
+    public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+        return inputFrame.rgba();
+    }
+}
