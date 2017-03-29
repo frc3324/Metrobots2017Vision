17a417efc9596e1be2784828cd68479cee5b5b10
YuyiChang <changyuyi98@qq.com>
2017-03-29 13:07:13 -0400

    Set app logo, add option menu, changeable app theme

--- app/src/main/java/org/metrobots/botcv/MainActivity.java
+++ app/src/main/java/org/metrobots/botcv/MainActivity.java
@@ -2,12 +2,17 @@ package org.metrobots.botcv;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.hardware.usb.UsbManager;
 import android.net.ConnectivityManager;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.SurfaceView;
 import android.widget.Toast;
 
@@ -33,8 +38,9 @@ public class MainActivity extends AppCompatActivity {
     private LimiterSlider limiterSlider = new LimiterSlider();
     private CameraImpl cameraImpl = new CameraImpl();
     private PeripheralManager peripheralManager;
-
+    private boolean isAllianceRed = true;
     private final String TAG = "MainActivity";
+    //private int hsvVal[3] = new getCenterHsv(); // TODO: Get center HSV value from cameraImpl
 
     static {
         System.loadLibrary("opencv_java3");
@@ -62,6 +68,40 @@ public class MainActivity extends AppCompatActivity {
             //Socket sock = new Socket();
             //System.out.println(sock.getRemoteSocketAddress().toString());
         } catch (Exception e) { e.printStackTrace(); }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Get which elements in the menu was clicked
+        int id = item.getItemId();
+        String TAG = "Menu Item";
+
+        switch (id) {
+            case R.id.first_item:
+                Log.i(TAG,"3324!");
+                break;
+            case R.id.color_change:
+                Log.i(TAG,"Change color!");
+                if(isAllianceRed) {
+                    getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.blueAlliance)));
+                    isAllianceRed = !isAllianceRed;
+                }
+                else {
+                    getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.redAlliance)));
+                }
+
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     /*private void connectUSB() {
@@ -101,4 +141,6 @@ public class MainActivity extends AppCompatActivity {
     public CameraImpl getCameraImpl() {
         return cameraImpl;
     }
+
+
 }
